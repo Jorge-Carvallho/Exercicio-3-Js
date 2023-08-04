5aabd4f17374f225c6f125f293a344a55e1c7753
Jorge-Carvallho <138339721+Jorge-Carvallho@users.noreply.github.com>
2023-08-03 23:09:35 -0300

    Adicionado link de inicializaçõao do exercício

--- conversor.js
+++ conversor.js
@@ -1,33 +1,35 @@
-const medida = prompt("Ensira uma medida em metros: ");
+function exibirPromptAlert() {
+  const medida = prompt("Ensira uma medida em metros: ");
 
-const unidade = prompt(
-  "Para qual unidade de medida deseja converter? " +
-    "\n1. Milímetro (mm)" +
-    "\n2. Centímetro (cm)" +
-    "\n3.Decímetro (dm)" +
-    "\n4. Decâmetro (dam)" +
-    "\n5. Hectômetro (hm)" +
-    "\n6. Quilômetro (km)"
-);
+  const unidade = prompt(
+    "Para qual unidade de medida deseja converter? " +
+      "\n1. Milímetro (mm)" +
+      "\n2. Centímetro (cm)" +
+      "\n3.Decímetro (dm)" +
+      "\n4. Decâmetro (dam)" +
+      "\n5. Hectômetro (hm)" +
+      "\n6. Quilômetro (km)"
+  );
 
-switch (unidade) {
-  case "1":
-    alert("Resultado: " + medida + "metros = " + medida * 1000 + "mm");
-    break;
-  case "2":
-    alert("Resultado: " + medida + "metros = " + medida * 100 + "cm");
-    break;
-  case "3":
-    alert("Resultado: " + medida + "metros = " + medida * 10 + "dm");
-    break;
-  case "4":
-    alert("Resultado: " + medida + "metros = " + medida / 10 + "dam");
-    break;
-  case "5":
-    alert("Resultado: " + medida + "metros = " + medida / 100 + "hm");
-    break;
-  case "6":
-    alert("Resultado: " + medida + "metros = " + medida / 1000 + "km");
-  default:
-    alert(" Valor de entrada invalido");
+  switch (unidade) {
+    case "1":
+      alert("Resultado: " + medida + "metros = " + medida * 1000 + "mm");
+      break;
+    case "2":
+      alert("Resultado: " + medida + "metros = " + medida * 100 + "cm");
+      break;
+    case "3":
+      alert("Resultado: " + medida + "metros = " + medida * 10 + "dm");
+      break;
+    case "4":
+      alert("Resultado: " + medida + "metros = " + medida / 10 + "dam");
+      break;
+    case "5":
+      alert("Resultado: " + medida + "metros = " + medida / 100 + "hm");
+      break;
+    case "6":
+      alert("Resultado: " + medida + "metros = " + medida / 1000 + "km");
+    default:
+      alert(" Valor de entrada invalido");
+  }
 }
